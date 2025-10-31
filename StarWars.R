bef780f476b021e13a850e6f8adbd699a315a4ba
OmarAlsubaihi <o.alsubaihi2@newcastle.ac.uk>
2025-10-31 14:50:30 +0000

    Add a bar plot for species numbers

---
+++ StarWars.R
@@ -0,0 +1,9 @@
+install.packages("dplyr")
+library(dplyr)
+data("starwars")
+glimpse(starwars)
+library(ggplot2)
+starwars %>%
+  count(species) %>%
+  ggplot(aes(y = species, x = n)) + 
+  geom_col()
